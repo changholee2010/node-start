321c63ff80372f8b8472ede86b7fe303b33ace94
you <you@email.com>
2026-01-28 12:32:50 +0900

    f

--- chap07-express/etc/test.js
+++ chap07-express/etc/test.js
@@ -1,2 +1,8 @@
-const file = Buffer.from("í¤ë³´ë", "latin1").toString("utf-8");
-console.log(file);
+const crypto = require("crypto");
+const numAry = ["1111", "2222", "3333", "4444", "5555"];
+
+for (let num of numAry) {
+  // 암호화 비번.
+  let passwd = crypto.createHash("sha512").update(num).digest("base64");
+  console.log(num, passwd);
+}
